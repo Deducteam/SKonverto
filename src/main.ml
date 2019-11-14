7f77b3f1e8741d7e092017eb8d90974b3b438e3b
Mohamed Yacine EL HADDAD <elhaddadmohamedyacine@hotmail.com>
2019-11-14 13:27:45 +0100

    Added handling files

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,11 @@
 
+let spec =
+  Arg.align []
+
 let _ =
-  Printf.printf "Init test !\n"
+  let usage = Printf.sprintf "Usage: %s [OPTIONS] [FILES]" Sys.argv.(0) in
+  let files = ref [] in
+  Arg.parse spec (fun s -> files := s :: !files) usage;
+  files := List.rev !files;
+  let process_file _ = () in
+  List.iter process_file !files
